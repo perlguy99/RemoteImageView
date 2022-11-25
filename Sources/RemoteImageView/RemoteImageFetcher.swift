8bea9895818549e8a8c1add47d4273b2fdae6469
Brent Michalski <brent.michalski@gmail.com>
2022-11-25 14:34:31 -0600

    Added purge method

--- Sources/RemoteImageView/RemoteImageFetcher.swift
+++ Sources/RemoteImageView/RemoteImageFetcher.swift
@@ -52,4 +52,8 @@ public class RemoteImageFetcher: ObservableObject {
     public func getUrl() -> URL {
         return url
     }
+    
+    public func purge() {
+        imageData = Data()
+    }
 }
